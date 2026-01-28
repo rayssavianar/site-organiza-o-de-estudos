045d5591392016134d6fc55458d056e3ff603a4c
Rayssa Viana <rayssavianar@gmail.com>
2026-01-28 09:32:54 -0300

    Dia 2 do projeto

---
+++ app.js
@@ -0,0 +1,22 @@
+const formTarefa = document.getElementById('formTarefa');
+const entradaTarefa = document.getElementById('entradaTarefa');
+const listaTarefas = document.getElementById('listaTarefas');
+
+entradaTarefa.addEventListener('input', (event) => {
+    console.log("Digitando:", event.target.value)
+})
+
+formTarefa.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let valorEntradaTarefa = entradaTarefa.value.trim();
+
+    if (valorEntradaTarefa === '') {
+        alert('Por favor, insira uma tarefa válida.');
+        return;
+    }
+
+    console.log(`Termo buscado: ${valorEntradaTarefa}`);
+
+    entradaTarefa.value = '';
+    entradaTarefa.focus();
+});
